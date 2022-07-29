901dd2def696c10af9ce786e68b0a25800f0f292
MrHalley <38929127+MrHalley@users.noreply.github.com>
2022-07-29 14:57:38 +0800

    业务代码: SPU管理、商品管理
    接口：
    
    【SPU管理】
      1.SPU列表条件搜索
    【商品管理】
      1.商品管理条件搜索

--- product/src/main/java/com/example/product/controller/SkuInfoController.java
+++ product/src/main/java/com/example/product/controller/SkuInfoController.java
@@ -31,11 +31,11 @@ public class SkuInfoController {
     private SkuInfoService skuInfoService;
 
     /**
-     * 列表
+     * 商品管理条件搜索
      */
     @RequestMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = skuInfoService.queryPage(params);
+        PageUtils page = skuInfoService.queryPageByCondition(params);
 
         return R.ok().put("page", page);
     }
--- product/src/main/java/com/example/product/controller/SpuInfoController.java
+++ product/src/main/java/com/example/product/controller/SpuInfoController.java
@@ -32,11 +32,11 @@ public class SpuInfoController {
     private SpuInfoService spuInfoService;
 
     /**
-     * 列表
+     * SPU列表条件搜索
      */
     @RequestMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = spuInfoService.queryPage(params);
+        PageUtils page = spuInfoService.queryPageByCondition(params);
 
         return R.ok().put("page", page);
     }
